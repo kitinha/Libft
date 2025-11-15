5229f1d2a79850713029066b3bab606ba53c17c8
kitinha <sm4sh200@gmail.com>
2025-11-15 18:03:22 +0000

    split

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/15 17:52:35 by codespace         #+#    #+#             */
-/*   Updated: 2025/11/15 17:52:44 by codespace        ###   ########.fr       */
+/*   Updated: 2025/11/15 18:03:09 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,75 +14,98 @@
 
 static int countwords(char *s, char c)
 {
-    int    i;
-    int    count;
+	int    i;
+	int    count;
 
-    i = 0;
-    count = 0;
-    while (s[i])
-    {
-        while (s[i] == c)
-            i++;
-        if (s[i] != '\0')
-          count++;
-        while (s[i] && s[i] != c)
-          i++;
-    }
-    return (count);
+	i = 0;
+	count = 0;
+	while (s[i])
+	{
+		while (s[i] == c)
+			i++;
+		if (s[i] != '\0')
+			count++;
+		while (s[i] && s[i] != c)
+		  i++;
+	}
+	return (count);
 }
 
 char **ft_split(const char *s, char c)
 {
 	char	**ns;
 	int		i;
-    int     j;
+	int		j;
 
-    j = 0;
+	j = 0;
 	i = 0;
 	ns = malloc(countwords(s, c) + 1 * sizeof (char *));
 	if (!ns)
 		return (NULL);
 	while (i < countwords(s, c))
 	{
-        while (s[j] == c)
-            j++;
-            ns[i] = fillwords(&s[j], c);
-            j += ft_strlen(ns[i]);
-
+		while (s[j] == c)
+			j++;
+		ns[i] = fillwords(&s[j], c);
+		if (!ns[i])
+		{	
+			ft_free(ns);
+			return (NULL);
+		}
+		j += ft_strlen(ns[i]);
 	}
 	return (ns);
 }
+static void ft_free(char **s)
+{
+	int i;
 
+	i = 0;
+	while (s[i])
+	{
+		free(s[i]);
+		i++;
+	}
+	free(s);
+}
 static char	*fillwords(char *s, char c)
 {
-    int     i;
-    int     len;
-    int     j;
-    char    *word;
+	int     i;
+	int     len;
+	int     j;
+	char    *word;
 
-    len = 0;
-    i = 0;
-    j = 0;
-    while (s[i] == c)
-        i++;
-    while (s[len + i] && s[len + i] != c)
-        len++;
-    word = malloc((len + 1) * sizeof(char));
-    if (!word)
-        return (NULL);
-    while (j < len)
-    {
-        word[j] = s[i];
-        j++;
-        i++;
-    }
-    word[j] = '\0';
-    return (word);
+	len = 0;
+	i = 0;
+	j = 0;
+	while (s[i] == c)
+		i++;
+	while (s[len + i] && s[len + i] != c)
+		len++;
+	word = malloc((len + 1) * sizeof(char));
+	if (!word)
+		return (NULL);
+	while (j < len)
+	{
+		word[j] = s[i];
+		j++;
+		i++;
+	}
+	word[j] = '\0';
+	return (word);
 }
 
 int	main(void)
 {
 	char	*s = ",,Hello, World, How areyou,,";
-    char c = ',';
-	printf("%", ft_split(s, c));
+	char c = ',';
+	char **result = ft_split(s, c);
+	int	i = 0;
+	while (result[i])
+	{
+		printf("%s", result[i]);
+		i++;
+	}
+	free (result);
+	return (0);
 }
